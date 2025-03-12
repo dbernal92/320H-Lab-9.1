cc91bb54f66768c581e6056717672fcb32067081
Darien Bernal <darien.bernal@gmail.com>
2025-03-12 00:13:23 -0400

    ToDoListItem displays the todo text and the checkbox marks completion

--- react-todo-list/src/components/ToDoListItem.jsx
+++ react-todo-list/src/components/ToDoListItem.jsx
@@ -4,9 +4,17 @@ import Button from "./Button";
 function ToDoListItem() {
     return (
         <div>
-            <Checkbox />
+            <Checkbox 
+                checked={todo.completed} 
+                onChange={() => dispatch({ type: "COMPLETE_TODO", index })}
+            />
+            <span>{todo.text}</span>
             <Button label="Edit" />
-            <Button label="Delete" disabled={!completed} />
+            <Button 
+                label="Delete" 
+                disabled={!todo.completed} 
+                onClick={() => dispatch({ type: "REMOVE_TODO", index })} 
+            />
         </div>
     )
 }
